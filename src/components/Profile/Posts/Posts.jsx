dfe7f98f6093b2123a76a0b58e4faceadd64749a
TonyKvint <shiponmyleg@gmail.com>
2022-05-21 09:59:02 +0300

    Used .map method in <Posts> component

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -1,12 +1,17 @@
 import classes from "./Posts.module.css";
 import Post from "./Post/Post";
 
-/* Массив данных, позже он будет вынесен отдельно, не пугайся */
+/* -------------------------------------------------------------- */
 
 let postData = [
-  {id: 1, message: "Hi, how are you?", likesCount: 19},
-  {id: 1, message: "It's my first post", likesCount: 5}
+  {id: 1, message: "Hi, how are you?", likesCount: 2},
+  {id: 2, message: "It's my first post", likesCount: 19},
+  {id: 3, message: "Hey, everyone!", likesCount: 5}
 ];
+
+let postElements = postData.map((p) => (
+  <Post message={p.message} id={p.id} likes={p.likesCount} />
+));
 
 /* -------------------------------------------------------------- */
 
@@ -14,11 +19,14 @@ const Posts = () => {
   return (
     <div>
       <div className={classes.messageInputBlock}>
-        <div><textarea placeholder="Write a message..."></textarea></div>
-        <div><button>Send</button></div>
+        <div>
+          <textarea placeholder="Write a message..."></textarea>
+        </div>
+        <div>
+          <button>Send</button>
+        </div>
       </div>
-      <Post message={postData[0].message} id={postData[0].id} likes={postData[0].likesCount} />
-      <Post message={postData[1].message} id={postData[1].id} likes={postData[1].likesCount} />
+      {postElements}
     </div>
   );
 };
